1c77f9ed6aaa55c3cd8e44068566af2245a47553
Kurt-Maglalang <160979583+Kurt-Maglalang@users.noreply.github.com>
2024-04-28 18:46:49 +0800

    Updated LR2_2.py, made files in problem 2 into txt

--- Lab 1/Problem 2/LR2_2.py
+++ Lab 1/Problem 2/LR2_2.py
@@ -4,19 +4,48 @@ inputs the lines of text into a list. The program then enters a loop in which it
 the user for a line number. Actual line numbers range from 1 to the number of lines in the file. If the input is 0, the program quits.
 Otherwise, the program prints the line associated with that number.9
 """ 
+import os
 
-# Prompt File Name & Open File
-fileName = input("Please enter File Name: ")
-file = open(fileName)
+def main():
+    fileFound = False
+    
+    # Prompt for filename
+    while not fileFound:
+        filename = input("Enter the filename (including path if necessary): ")
 
-# Pass Lines of text into a list
-lines = file.readlines()
+        try:
+            # Open the file and read lines into a list
+            with open(filename, 'r') as file:
+                lines = file.readlines()
+                fileFound = True
+        except FileNotFoundError:
+            print("File not found.")
+        except PermissionError:
+            print("Permission denied. You might not have the necessary permissions to access this file.")
+        except Exception as e:
+            print("An error occurred:", str(e))
 
-# Prompt user for a line number (1 and above, 0 to quit)
-while True:
-    print("Total Number of Lines in Text file: ", len(lines))
-    lineNum = input("Please enter a line number: ")
-    if lineNum == 0:
-        break
-    lineNum = int(lineNum) - 1
-    print(lines[lineNum])
+    num_lines = len(lines)
+
+    while True:
+        print("\nNumber of lines in the file:", num_lines)
+        line_num = input("Enter a line number (1 to {}), or 0 to quit: ".format(num_lines))
+        
+        # Check if the input is a valid integer
+        if not line_num.isdigit():
+            print("Invalid input. Please enter a number.")
+            continue
+        
+        line_num = int(line_num)
+
+        if line_num == 0:
+            print("Exiting program.")
+            break
+        elif line_num < 1 or line_num > num_lines:
+            print("Invalid line number. Please enter a number between 1 and {}.".format(num_lines))
+        else:
+            print("Line {}:".format(line_num))
+            print(lines[line_num - 1].strip())
+
+if __name__ == "__main__":
+    main()
